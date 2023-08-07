17a53e5253c29f538666ba7bb126d30b865a78ad
jrfm <moesch.jonas@protonmail.com>
2023-08-07 20:22:10 +0200

    clean up project and add browser-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>jrfm</div>
+      <div>Hello World</div>
+      {/* <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/balkenBewehrung" element={<BeamReinforcement />} />
+        <Route path="/anchorageLength" element={<AnchorageLength />} />
+      </Routes> */}
     </>
   );
 }
