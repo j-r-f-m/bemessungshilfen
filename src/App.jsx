32fc7ec7cb4e6f8ab47018d91b683dd049436117
jrfm <moesch.jonas@protonmail.com>
2023-08-07 20:33:43 +0200

    add Landing.jsx and corresponding packeges

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/Landing";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/balkenBewehrung" element={<BeamReinforcement />} />
-        <Route path="/anchorageLength" element={<AnchorageLength />} />
-      </Routes> */}
+        {/* <Route path="/balkenBewehrung" element={<BeamReinforcement />} />
+        <Route path="/anchorageLength" element={<AnchorageLength />} /> */}
+      </Routes>
     </>
   );
 }
